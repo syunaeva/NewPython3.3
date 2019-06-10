5d9e30c69f2fa5165ed368dfb0cf98bdecfc35d9
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2019-06-09 17:47:10 -0700

    Functions examples Python 3.3

---
+++ Functions.py
@@ -0,0 +1,21 @@
+def example1(arg1, arg2, *, different=None):
+    """
+    exampleof keyword-only arguments
+
+    used for expanding pre-existing APIs by passing
+    more agruments than expected
+    """
+    pass
+
+spam = None
+bacon = 42
+
+
+def monty_python(a:spam, b:bacon) -> "different":
+    """
+    TODO: find more info and examples
+    :param a:
+    :param b:
+    :return:
+    """
+    pass
