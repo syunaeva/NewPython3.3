a81422b2e458cb906abaf5dac7e7ac7790f50851
Safiya Yunaeva <syunaeva@SYHomeMacBookPro.sweethome>
2019-06-09 17:17:45 -0700

    Exception examples in Python 3.3

--- Exceptions.py
+++ Exceptions.py
@@ -13,3 +13,27 @@ def example1():
         raise Exception
     except Exception as exc:
         traceback.print_tb(exc.__traceback__)
+
+
+def example2():
+    """
+    implicit exception chain
+    """
+    try:
+        raise Exception
+    except Exception:
+        raise NotImplementedError # __context__ set - no loss date during debugging
+
+def example3():
+    """
+    explicit exception chain
+    :return:
+    """
+    try:
+        raise Exception
+    except Exception as exc:
+        # Sets __cause__
+        # In Python 3.3, ''from None''supresses
+        raise NotImplementedError from exc
+
+example3()
